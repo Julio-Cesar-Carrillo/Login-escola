a7cfd40e1dce4904862001f58899568edc46f814
unknown <Julio-Cesar-Carrillo>
2023-10-25 20:09:31 +0200

    cambios check.php - 25.10.2023 - 20:09

--- check.php
+++ check.php
@@ -1,5 +1,12 @@
 <?php
-    // Incluimos el archivo de conexión a la base de datos.
+if (filter_has_var(INPUT_POST, 'EnvioCheck')) 
+{
+    header('Location: ' . './index.php?error=Debes rellenar el formulario');
+    exit();
+} 
+
+else 
+{ // Incluimos el archivo de conexión a la base de datos.
     include('conexion.php');
 
     $user = $_POST['user'];
@@ -9,24 +16,21 @@
     $consulta = "SELECT * FROM tbl_profesores WHERE nombre_profe = '$user'";
     $resultado = $conn->query($consulta);
 
-    if ($resultado->num_rows === 1) 
-    {
+    if ($resultado->num_rows === 1) {
         $fila = $resultado->fetch_assoc();
         $contra_profe = $fila['contra_profe']; // CONTRASEÑA QUE TIENE EL USUARIO
         $contra_encriptada = hash("sha256", $contra); // ENCRIPTADO DE LA CONTRASEÑA DEL FORMULARIO
 
         // Comprueba si la contraseña es correcta
-        if ((hash_equals($contra_encriptada, $contra_profe))) 
-        {
+        if ((hash_equals($contra_encriptada, $contra_profe))) {
             echo "¡Bienvenido, $user! La contraseña es correcta.";
-        } 
-    } 
-    
-    else 
-    {
-        header('Location: '.'./index.php?error=Usuario o contraseña incorrectas');
-    }
+        }
 
+        else
+        {
+            header('Location: '.'./index.php?Usuario o contraseña incorrectos');
+        }
+    }
     // Cierra la conexión a la base de datos
     $conn->close();
-?>
+}
