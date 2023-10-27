42f0382bc70753944b9df0735d0a91a614aa7c70
IkerCG2003 <100007043.joan23@fje.edu>
2023-10-27 16:15:20 +0200

    Añadir SMTP para evitar inyecciones SQL - 27.10.2023

--- procesos/check.php
+++ procesos/check.php
@@ -1,41 +1,50 @@
 <?php
-    if (!filter_has_var(INPUT_POST, 'EnvioCheck')) // Comprueba si en el POST existe EnvioCheck
-    {
-        header('Location: ../index.php?error=Debes rellenar el formulario para acceder a check.php'); // Si no existe te redirige a index.php con un mensaje de error.
-        exit();
-    } 
+if (!filter_has_var(INPUT_POST, 'EnvioCheck')) // Comprueba si el campo 'EnvioCheck' existe en la solicitud POST
+{
     
-    else 
+    header('Location: ../index.php?error=Debes rellenar el formulario para acceder a check.php'); // Si no existe, redirige al usuario a index.php con un mensaje de error y termina el script.
+    exit();
+} 
+
+else 
+{
+    session_start(); // Iniciamos la sesión.
+
+    $contra = $_POST['contra']; // Obtenemos la contraseña del formulario.
+
+    include('conexion.php'); // Incluimos el archivo de conexión a la base de datos.
+
+    $consulta = "SELECT * FROM tbl_profesores WHERE nombre_profe = ?"; // Consulta SQL con marcador de posición.
+
+    if ($stmt = $conn->prepare($consulta)) // Preparamos la consulta SQL con un marcador de posición.
     {
-        session_start(); // Iniciamos la sesión.
+        $stmt->bind_param('s', $_SESSION['user']); // Vinculamos el valor de $_SESSION['user'] al marcador de posición.
+        
+        $stmt->execute(); // Ejecutamos la consulta preparada.
+        
+        $result = $stmt->get_result(); // Obtenemos el resultado de la consulta.
 
-        $contra = $_POST['contra'];
-
-        // Incluimos el archivo de conexión a la base de datos.
-        include('conexion.php');
-
-        // Consulta SQL para buscar el usuario en la base de datos
-        $consulta = "SELECT * FROM tbl_profesores WHERE nombre_profe = '" . $_SESSION['user'] . "'"; // Crea una consulta a la base de datos con los parámetros.
-        $resultado = $conn->query($consulta); // Realiza la consulta.
-
-        if ($resultado->num_rows === 1) // Si un resultado coincide comprueba si la contraseña introducida y la asignada al profesor son la misma.
+        if ($result->num_rows === 1) 
         {
-            $fila = $resultado->fetch_assoc();
-            $contra_profe = $fila['contra_profe']; // CONTRASEÑA QUE TIENE EL USUARIO
-            $contra_encriptada = hash("sha256", $contra); // ENCRIPTADO DE LA CONTRASEÑA DEL FORMULARIO
+            $fila = $result->fetch_assoc(); // Si encontramos una fila que coincide con el usuario en la base de datos.
 
-            // Comprueba si la contraseña es correcta
-            if (hash_equals($contra_encriptada, $contra_profe)) 
-            {
-                echo "¡Bienvenido, " . $_SESSION['user'] . "! La contraseña es correcta."; // Si lo son te da la bienvenida.
-            }
+            $contra_profe = $fila['contra_profe'];  // Obtenemos la contraseña almacenada en la base de datos.
 
-            else
-            {   
-                header('Location: ../index.php?error=Usuario o contraseña incorrectos'); // Si no lo son te redirige a index.php con un mensaje de error.
+            $contra_encriptada = hash("sha256", $contra); // Encriptamos la contraseña proporcionada por el usuario.
+
+            if (hash_equals($contra_encriptada, $contra_profe)) // Comparamos la contraseña encriptada del usuario con la almacenada en la base de datos.
+            {  
+                echo "¡Bienvenido, " . $_SESSION['user'] . "! La contraseña es correcta."; // Si coinciden, se da la bienvenida.
+                
+            } 
+            
+            else {
+                header('Location: ../index.php?error=Usuario o contraseña incorrectos'); // Si no coinciden, redirige al usuario a index.php con un mensaje de error.
                 exit();
             }
-        } 
-        $conn->close();   
+        }
+        $stmt->close(); // Cerramos la consulta preparada.
     }
+    $conn->close(); // Cerramos la conexión a la base de datos.
+}
 ?>
