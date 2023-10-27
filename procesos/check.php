c7c6cf9869bef482a3b44cec5ac4e6480ebfc464
IkerCG2003 <100007043.joan23@fje.edu>
2023-10-27 18:04:26 +0200

    Comentarios de los cambios realizados - 27.10.2023

--- procesos/check.php
+++ procesos/check.php
@@ -1,6 +1,7 @@
 <?php
     session_start(); // Iniciamos la sesión.
 
+    // Verifica si las variables de sesión 'user' y 'pass' están definidas
     if (!isset($_SESSION['user']) || !isset($_SESSION['pass'])) 
     {
         header('Location: ../index.php?error=Debes rellenar el formulario para acceder a check.php');
@@ -9,6 +10,7 @@
 
     include('conexion.php'); // Incluimos el archivo de conexión a la base de datos.
 
+    // Preparamos una consulta SQL para buscar un profesor por nombre
     $consulta = "SELECT * FROM tbl_profesores WHERE nombre_profe = ?";
     if ($stmt = $conn->prepare($consulta))
     {
@@ -16,6 +18,7 @@
         $stmt->execute();
         $result = $stmt->get_result();
 
+        // Si se encuentra un resultado (1 fila), verificamos la contraseña
         if ($result->num_rows === 1) 
         {
             $fila = $result->fetch_assoc();
@@ -23,19 +26,22 @@
             $pass = $_SESSION['pass'];
             $contra_encriptada = hash("sha256", $pass);
 
+            // Comparamos la contraseña almacenada con la contraseña proporcionada
             if (hash_equals($contra_encriptada, $contra_profe)) 
             {
+                // La contraseña es correcta, muestra un mensaje de bienvenida
                 echo "¡Bienvenido, " . $_SESSION['user'] . "! La contraseña es CORRECTA. <br>";
             } 
             
             else 
             {
-                header('Location: ../index.php?Usuario o contraseña INCORRECTAS');
+                // Redirige de vuelta a la página de inicio en caso de usuario o contraseña incorrectas
+                header('Location: ../index.php?error=Usuario o contraseña INCORRECTAS');
             }
         }
 
         $stmt->close();
     }
 
-    $conn->close();
+    $conn->close(); // Cierra la conexión a la base de datos
 ?>
--- procesos/validacion.php
+++ procesos/validacion.php
@@ -1,23 +1,24 @@
 <?php
     session_start(); // Iniciamos la sesión.
 
-    if (!isset($_POST['enviar'])) 
-    {
+    // Verifica si el formulario se ha enviado
+    if (!isset($_POST['enviar'])) {
         header('Location: ../index.php?error=Debes rellenar el formulario para acceder a validacion.php');
         exit();
     }
 
-    if (isset($_POST['user'])) 
-    {
+    // Si se ha enviado un valor para 'user', lo almacenamos en una variable de sesión llamada 'user'
+    if (isset($_POST['user'])) {
         $_SESSION['user'] = $_POST['user'];
     }
 
-    if (isset($_POST['pass'])) 
-    {
+    // Si se ha enviado un valor para 'pass', lo almacenamos en una variable de sesión llamada 'pass'
+    if (isset($_POST['pass'])) {
         $pass = $_POST['pass'];
         $_SESSION['pass'] = $pass;
     }
 
+    // Redirige al usuario a la página 'check.php'
     header("Location: check.php");
     exit();
 ?>
