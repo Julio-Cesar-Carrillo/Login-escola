74ff9302efc5bfd46e4cca8cd5a41401fb1775ed
IkerCG2003 <100007043.joan23@fje.edu>
2023-10-27 16:10:13 +0200

    Implementación de sesiones - 27.10.2023 - 16:10

--- procesos/check.php
+++ procesos/check.php
@@ -1,37 +1,41 @@
 <?php
-    if (!filter_has_var(INPUT_POST, 'EnvioCheck')) {
-        header('Location: ../index.php?error=Debes rellenar el formulario para acceder a check.php');
+    if (!filter_has_var(INPUT_POST, 'EnvioCheck')) // Comprueba si en el POST existe EnvioCheck
+    {
+        header('Location: ../index.php?error=Debes rellenar el formulario para acceder a check.php'); // Si no existe te redirige a index.php con un mensaje de error.
         exit();
     } 
     
     else 
     {
-    // Incluimos el archivo de conexión a la base de datos.
-    include('conexion.php');
+        session_start(); // Iniciamos la sesión.
 
-    $user = $_POST['user'];
-    $contra = $_POST['contra'];
+        $contra = $_POST['contra'];
 
-    // Consulta SQL para buscar el usuario en la base de datos
-    $consulta = "SELECT * FROM tbl_profesores WHERE nombre_profe = '$user'";
-    $resultado = $conn->query($consulta);
+        // Incluimos el archivo de conexión a la base de datos.
+        include('conexion.php');
 
-    if ($resultado->num_rows === 1) {
-        $fila = $resultado->fetch_assoc();
-        $contra_profe = $fila['contra_profe']; // CONTRASEÑA QUE TIENE EL USUARIO
-        $contra_encriptada = hash("sha256", $contra); // ENCRIPTADO DE LA CONTRASEÑA DEL FORMULARIO
+        // Consulta SQL para buscar el usuario en la base de datos
+        $consulta = "SELECT * FROM tbl_profesores WHERE nombre_profe = '" . $_SESSION['user'] . "'"; // Crea una consulta a la base de datos con los parámetros.
+        $resultado = $conn->query($consulta); // Realiza la consulta.
 
-        // Comprueba si la contraseña es correcta
-        if ((hash_equals($contra_encriptada, $contra_profe))) {
-            echo "¡Bienvenido, $user! La contraseña es correcta.";
-        }
-
-        else
+        if ($resultado->num_rows === 1) // Si un resultado coincide comprueba si la contraseña introducida y la asignada al profesor son la misma.
         {
-            header('Location: '.'../index.php?Usuario o contraseña incorrectos');
-            exit();
-        }
-    } 
-    $conn->close();   
-}
+            $fila = $resultado->fetch_assoc();
+            $contra_profe = $fila['contra_profe']; // CONTRASEÑA QUE TIENE EL USUARIO
+            $contra_encriptada = hash("sha256", $contra); // ENCRIPTADO DE LA CONTRASEÑA DEL FORMULARIO
+
+            // Comprueba si la contraseña es correcta
+            if (hash_equals($contra_encriptada, $contra_profe)) 
+            {
+                echo "¡Bienvenido, " . $_SESSION['user'] . "! La contraseña es correcta."; // Si lo son te da la bienvenida.
+            }
+
+            else
+            {   
+                header('Location: ../index.php?error=Usuario o contraseña incorrectos'); // Si no lo son te redirige a index.php con un mensaje de error.
+                exit();
+            }
+        } 
+        $conn->close();   
+    }
 ?>
--- procesos/validacion.php
+++ procesos/validacion.php
@@ -1,14 +1,18 @@
 <?php
-    if (!filter_has_var(INPUT_POST, 'enviar')) {
+    if (!filter_has_var(INPUT_POST, 'enviar')) 
+    {
         header('Location: ../index.php?error=Debes de rellenar el formulario para acceder a validacion.php');
         exit();
-    } else {
-        $user = $_POST['user'];
-        $contra = $_POST['pass'];
+    } 
+    
+    else 
+    {
+        session_start(); // Iniciamos la sesión.
+
+        $_SESSION['user'] = isset($_POST['user']) ? $_POST['user'] : ""; // Creamos la variable de sesión a partir del formulario.
 
         // Pasa el usuario y la contraseña a check.php utilizando una solicitud POST
         echo "<form id='EnvioCheck' name='EnvioCheck' action='check.php' method='POST'>";
-        echo "<input type='hidden' name='user' value='" . $user . "'>";
         echo "<input type='hidden' name='contra' value='" . $contra . "'>";
         echo "<input type='hidden' name='EnvioCheck' value='1'>"; // Agrega una bandera para indicar que esta es una presentación de formulario
         echo "</form>";
