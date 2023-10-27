922b266a3d38f9d37ecd7a76d0e37f3e21a05911
Óscar <oscarlm27@gmail.com>
2023-10-27 18:32:30 +0200

    actua

--- procesos/cerrar_sesion.php
+++ procesos/cerrar_sesion.php
@@ -9,16 +9,39 @@
     {
         // Iniciamos sesion para poder trabajar con las variables $_SESSION
         session_start();
-
-        echo "Adiós " . $_SESSION['user'] . "<br>"; // Se despide del usuario cogiendo la variable de sesión
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cerrar sessión</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    
+<div class="c_sesion">
+        <p>Adiós <?php echo $_SESSION['user']; ?>, hasta la próxima!<br></p> <!-- Se despide del usuario cogiendo la variable de sesión -->
         
-        echo "<a href='../index.php'>Volver</a>"; // Te redirige a index.php
+        <p id="carga">Cerrando sesion, porfavor, espere...</p><br> <!-- Te redirige a index.php -->
 
+        <a href='../index.php'>
+            <img src='../img/danieh.jpeg' width='10%'>
+            <script>
+                setTimeout(function () {
+                // Redirigir con JavaScript
+                window.location.href= '../index.php';
+                }, 3000);
+            </script>
+        </a>
+</div>
+</body>
+</html>
+<?php
         // Destruir todas las variables de sesion
         session_unset();
 
         // Destruir la sesión
         session_destroy();
     }
-    exit();
 ?>
